210b0ffa21d67a77207d3934485255d4be48580f
burness <burness1990@gmail.com>
2015-03-21 23:03:26 +0800

    delete flickr and add comrite

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'Comrite', 'url': 'http://login.comrite.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 if os.environ.get('DATABASE_URL') is None:
